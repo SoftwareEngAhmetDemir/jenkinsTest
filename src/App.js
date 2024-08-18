79d784802957828a5ce62b47f22fac4461efca67
Ahmet <a.demir@extratik.com>
2024-08-18 19:19:41 +0300

    sy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir !!!! lastest https
+          Learn React with ahmet demir !!!! lastest syria
         </a>
       </header>
     </div>
