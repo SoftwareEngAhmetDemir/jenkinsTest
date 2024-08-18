e9643cd294a4dd49f6949540becac1eaec4c43e4
Ahmet <a.demir@extratik.com>
2024-08-18 18:02:23 +0300

    german

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir !!!! hhh
+          Learn React with ahmet demir !!!! german
         </a>
       </header>
     </div>
