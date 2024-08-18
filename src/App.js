2d0c99220705821461d3a4a3d597bf6c966f4a8c
Ahmet <a.demir@extratik.com>
2024-08-18 17:40:12 +0300

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir lord
+          Learn React with ahmet demir lord of the rings !!!!
         </a>
       </header>
     </div>
