2d17d06b0c47cf1d733c11b167f41eb3eedbda81
Ahmet <a.demir@extratik.com>
2024-08-18 17:45:30 +0300

    fixing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir !!!!
+          Learn React with ahmet demir !!!! hhh
         </a>
       </header>
     </div>
