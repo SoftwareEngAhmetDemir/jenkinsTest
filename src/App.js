3baf6c6344970348d2f3c5acc49d02a6fe747430
Ahmet <a.demir@extratik.com>
2024-08-18 23:40:03 +0300

    looks

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir !!!! lastest remove
+          Learn React with ahmet demir !!!! lastest remove looks
         </a>
       </header>
     </div>
