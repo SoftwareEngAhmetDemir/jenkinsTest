94ff097f2f2cf96242be53afcd8647008a5899c7
Ahmet <a.demir@extratik.com>
2024-08-19 15:58:54 +0300

    xxd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir !!!! alyona return erden xx roat
+          Learn React with ahmet demir !!!! alyona return 
         </a>
       </header>
     </div>
