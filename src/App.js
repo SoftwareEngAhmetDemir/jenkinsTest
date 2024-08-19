2e1d4859a7e1bd1a972fa32074e835d152ce21d4
Ahmet <a.demir@extratik.com>
2024-08-19 15:51:38 +0300

    alyona

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with ahmet demir !!!! pipe
+          Learn React with ahmet demir !!!! pip
         </a>
       </header>
     </div>
